09982ae23ce3ecc454153f2c2c6d7fd4cf02f30d
Anastasia Zizeko <nastjaznf@gmail.com>
2020-02-21 17:33:47 +0300

    refactor: remove unnesessary commits

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,3 @@
-//let arrOfDepthLevels = [];
 let counter = 0;
 module.exports = class DepthCalculator {
     calculateDepth(arr, level = 1, arrOfDepthLevels = []) {
@@ -7,13 +6,6 @@ module.exports = class DepthCalculator {
         for (let i = 0; i < arr.length; i++) {
             if (Array.isArray(arr[i])) this.calculateDepth(arr[i], level + 1, arrOfDepthLevels);
         }
-        //counter = Math.max(...arrOfDepthLevels);
-        /* counter = arrOfDepthLevels[0];
-        for (let i = 1; i < arrOfDepthLevels.length; i++) {
-            if (arrOfDepthLevels[i] > counter) {
-                counter = arrOfDepthLevels[i];
-            }
-        } */
         counter = Math.max(...arrOfDepthLevels);
         return counter;
 
