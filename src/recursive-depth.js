767c97c4173adabf6d1cffd0d38b6c948b72b1c3
Anastasia Zizeko <nastjaznf@gmail.com>
2020-02-21 17:30:10 +0300

    feat: solve recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,21 @@
+//let arrOfDepthLevels = [];
+let counter = 0;
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, level = 1, arrOfDepthLevels = []) {
+
+        arrOfDepthLevels.push(level);
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) this.calculateDepth(arr[i], level + 1, arrOfDepthLevels);
+        }
+        //counter = Math.max(...arrOfDepthLevels);
+        /* counter = arrOfDepthLevels[0];
+        for (let i = 1; i < arrOfDepthLevels.length; i++) {
+            if (arrOfDepthLevels[i] > counter) {
+                counter = arrOfDepthLevels[i];
+            }
+        } */
+        counter = Math.max(...arrOfDepthLevels);
+        return counter;
+
     }
-};
+}
